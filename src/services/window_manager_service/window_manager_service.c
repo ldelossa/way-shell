891d246cf8dc35de4936fd5b1e1ef73cf70e2e98
ldelossa <louis.delos@gmail.com>
2024-06-29 10:11:57 -0400

    wm: create window manager abstraction
    Introduce a struct and function pointer types for subsequent window
    manager abstraction.
    
    Signed-off-by: ldelossa <louis.delos@gmail.com>

--- src/services/window_manager_service/window_manager_service.c
+++ src/services/window_manager_service/window_manager_service.c
@@ -1,5 +1,29 @@
 #include "window_manager_service.h"
 
+#include "./sway/window_manager_service_sway.h"
+
+static WindowManager *global = NULL;
+
 char *WMWorkspaceEventStringTbl[WMWORKSPACE_EVENT_LEN] = {
     "CREATED", "DESTROYED", "FOCUSED", "MOVED", "RENAMED", "URGENT", "RELOAD",
 };
+
+// Initialize the window manager service
+int window_manager_service_init() {
+    GSettings *settings =
+        g_settings_new("org.ldelossa.way-shell.window-manager");
+
+    char *backend = g_settings_get_string(settings, "backend");
+
+    if (g_strcmp0(backend, "sway") == 0) {
+        global = wm_service_sway_window_manager_init();
+    } else {
+        g_warning("Unknown backend: %s", backend);
+        return -1;
+    }
+    return 0;
+}
+
+// Obtain the global window manager service, a call to
+// `window_manager_service_init` must be made before this.
+WindowManager *window_manager_service_get_global() { return global; }
