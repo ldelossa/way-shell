b33da5ee5aa4463b78d93418ea09f54d78c945e0
ldelossa <louis.delos@gmail.com>
2024-06-29 10:11:57 -0400

    wm: create window manager abstraction
    Introduce a struct and function pointer types for subsequent window
    manager abstraction.
    
    Signed-off-by: ldelossa <louis.delos@gmail.com>

--- src/services/window_manager_service/window_manager_service.h
+++ src/services/window_manager_service/window_manager_service.h
@@ -61,3 +61,73 @@ typedef struct _WMOutput {
     gchar *serial;
     gchar *current_workspace;
 } WMOutput;
+
+// WinowManager is a virtual function table which abstracts an underlying
+// window manager implementation.
+//
+// Each implementation should define a function which returns a WindowManager
+// with its method's set to internal function pointers.
+typedef struct _WindowManager WindowManager;
+typedef GPtrArray *(*wm_get_workspaces_func)(WindowManager *self);
+
+typedef GPtrArray *(*wm_get_outputs_func)(WindowManager *self);
+
+typedef int (*wm_focus_workspace_func)(WindowManager *self, WMWorkspace *ws);
+
+typedef int (*wm_current_ws_to_output_func)(WindowManager *self, WMOutput *o);
+
+typedef int (*wm_current_app_to_workspace_func)(WindowManager *self,
+                                                WMWorkspace *ws);
+
+typedef void (*wm_on_workspaces_changed)(WindowManager *self,
+                                         GPtrArray *workspaces, void *data);
+typedef void (*wm_on_outputs_changed)(WindowManager *self, GPtrArray *outputs,
+                                      void *data);
+
+typedef guint (*wm_register_on_workspaces_changed)(WindowManager *self,
+                                                   wm_on_workspaces_changed cb,
+                                                   void *data);
+
+typedef guint (*wm_unregister_on_workspaces_changed)(
+    WindowManager *self, wm_on_workspaces_changed cb, void *data);
+
+typedef guint (*wm_register_on_outputs_changed)(WindowManager *self,
+                                                wm_on_outputs_changed cb,
+                                                void *data);
+
+typedef guint (*wm_unregister_on_outputs_changed)(WindowManager *self,
+                                                  wm_on_outputs_changed cb,
+                                                  void *data);
+
+typedef struct _WindowManager {
+    // private struct which implements the WindowManager interface, interface
+    // methods below can cast this to their internal type.
+    void *private;
+    // Provide a list of currently available workspaces
+    wm_get_workspaces_func get_workspaces;
+    // Provide a list of currently available outputs
+    wm_get_outputs_func get_outputs;
+    // Focus the provided workspace
+    wm_focus_workspace_func focus_workspace;
+    // Move the current workspace to the specified output
+    wm_current_ws_to_output_func current_ws_to_output;
+    // Move the current app to the specified workspace
+    wm_current_app_to_workspace_func current_app_to_workspace;
+    // register a callback when workspaces has changed.
+    // returns the GObject signal ID on success.
+    wm_register_on_workspaces_changed register_on_workspaces_changed;
+    // unregister a callback when workspaces has changed.
+    wm_unregister_on_workspaces_changed unregister_on_workspaces_changed;
+    // register a callback when outputs has changed.
+    // returns the GObject signal ID on success.
+    wm_register_on_outputs_changed register_on_outputs_changed;
+    // unregister a callback when outputs has changed.
+    wm_unregister_on_outputs_changed unregister_on_outputs_changed;
+} WindowManager;
+
+// Initialize the window manager service
+int window_manager_service_init();
+
+// Obtain the global window manager service, a call to
+// `window_manager_service_init` must be made before this.
+WindowManager *window_manager_service_get_global();
