b609b0821108de4b1defede39b78ca76f4e6f11a
ldelossa <louis.delos@gmail.com>
2024-09-29 17:40:54 -0400

    refactor: handling dbusmenu
    After some experimentation:
    -  Crashes can be avoided when swapping out a newly updated GMenuModel
       into the GtkPopoverMenu by using the correct API (who would have
       guessed).
    
    Signed-off-by: ldelossa <louis.delos@gmail.com>

--- src/panel/indicator_bar/indicator_widget.c
+++ src/panel/indicator_bar/indicator_widget.c
@@ -4,7 +4,6 @@
 
 #include "../../services/status_notifier_service/status_notifier_item_dbus.h"
 #include "../../services/status_notifier_service/status_notifier_service.h"
-#include "../panel.h"
 
 struct _IndicatorWidget {
     GObject parent_instance;
@@ -17,11 +16,19 @@ struct _IndicatorWidget {
 };
 G_DEFINE_TYPE(IndicatorWidget, indicator_widget, G_TYPE_OBJECT);
 
+static void on_sni_menu_updated(StatusNotifierService *s,
+                                StatusNotifierItem *sni, IndicatorWidget *self);
+static void on_new_icon(DbusItemV0Gen *item, IndicatorWidget *self);
 // stub out dispose, finalize, class_init, and init methods
 static void indicator_widget_dispose(GObject *gobject) {
     IndicatorWidget *self = INDICATOR_WIDGET(gobject);
     // Chain-up
     G_OBJECT_CLASS(indicator_widget_parent_class)->dispose(gobject);
+    // kill signals on sni->proxy
+    g_signal_handlers_disconnect_by_func(self->sni->proxy, on_new_icon, self);
+
+    StatusNotifierService *s = status_notifier_service_get_global();
+    g_signal_handlers_disconnect_by_func(s, on_sni_menu_updated, self);
 };
 
 static void indicator_widget_finalize(GObject *gobject) {
@@ -35,15 +42,14 @@ static void indicator_widget_class_init(IndicatorWidgetClass *klass) {
     object_class->finalize = indicator_widget_finalize;
 };
 
-// static void on_button_clicked(GtkButton *button, IndicatorWidget *self) {
-//     GError *error = NULL;
-//     dbus_item_v0_gen_call_activate_sync(
-//         status_notifier_item_get_proxy(self->sni), 0, 0, NULL, &error);
-//     if (error) {
-//         g_warning("Failed to activate item: %s", error->message);
-//         g_error_free(error);
-//     }
-// }
+static void on_button_clicked(GtkButton *button, IndicatorWidget *self) {
+    GError *error = NULL;
+    dbus_item_v0_gen_call_activate_sync(
+        status_notifier_item_get_proxy(self->sni), 0, 0, NULL, &error);
+    if (error) {
+        g_error_free(error);
+    }
+}
 
 static void indicator_widget_init_layout(IndicatorWidget *self) {
     self->container = GTK_BOX(gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0));
@@ -58,8 +64,6 @@ static void indicator_widget_init_layout(IndicatorWidget *self) {
 
     self->button = GTK_BUTTON(gtk_button_new());
     gtk_button_set_child(self->button, GTK_WIDGET(self->icon));
-    // g_signal_connect(self->button, "clicked", G_CALLBACK(on_button_clicked),
-    //                  self);
 
     // wire it up
     gtk_box_append(self->box, GTK_WIDGET(self->button));
@@ -80,16 +84,13 @@ static void on_button_clicked_with_menu(GtkButton *button,
     status_notifier_item_about_to_show(self->sni, 0);
 }
 
-void indicator_widget_set_sni(IndicatorWidget *self, StatusNotifierItem *sni) {
-    self->sni = sni;
-
-    const gchar *icon_name = status_notifier_item_get_icon_name(sni);
+static void indicator_widget_set_icon(IndicatorWidget *self) {
+    const gchar *icon_name = status_notifier_item_get_icon_name(self->sni);
 
     if (icon_name && strlen(icon_name) > 0) {
-        gtk_image_set_from_icon_name(self->icon,
-                                     status_notifier_item_get_icon_name(sni));
+        gtk_image_set_from_icon_name(self->icon, icon_name);
     } else {
-        GdkPixbuf *pix = status_notifier_item_get_icon_pixmap(sni);
+        GdkPixbuf *pix = status_notifier_item_get_icon_pixmap(self->sni);
         if (pix) {
             GdkTexture *t = gdk_texture_new_for_pixbuf(pix);
             gtk_image_set_from_paintable(self->icon, GDK_PAINTABLE(t));
@@ -97,6 +98,37 @@ void indicator_widget_set_sni(IndicatorWidget *self, StatusNotifierItem *sni) {
             gtk_image_set_from_icon_name(self->icon, "image-missing");
         }
     }
+}
+
+static void on_new_icon(DbusItemV0Gen *sni, IndicatorWidget *self) {
+    g_debug("indicator_widget.c:on_new_icon() called");
+    indicator_widget_set_icon(self);
+}
+
+static void on_sni_menu_updated(StatusNotifierService *s,
+                                StatusNotifierItem *sni,
+                                IndicatorWidget *self) {
+    g_debug("indicator_widget.c:on_sni_menu_updated() called");
+    if (sni != self->sni) return;
+    gtk_popover_menu_set_menu_model(self->menu, G_MENU_MODEL(sni->menu_model));
+}
+
+void indicator_widget_set_sni(IndicatorWidget *self, StatusNotifierItem *sni) {
+    self->sni = sni;
+
+    indicator_widget_set_icon(self);
+    g_signal_connect(sni->proxy, "new-icon", G_CALLBACK(on_new_icon), self);
+    g_signal_connect(sni->proxy, "new-attention-icon", G_CALLBACK(on_new_icon),
+                     self);
+    g_signal_connect(sni->proxy, "new-overlay-icon", G_CALLBACK(on_new_icon),
+                     self);
+
+    if (sni->menu_model)
+        g_signal_connect(self->button, "clicked",
+                         G_CALLBACK(on_button_clicked_with_menu), self);
+    else
+        g_signal_connect(self->button, "clicked", G_CALLBACK(on_button_clicked),
+                         self);
 
     if (!sni->menu_model) return;
 
@@ -110,8 +142,9 @@ void indicator_widget_set_sni(IndicatorWidget *self, StatusNotifierItem *sni) {
     gtk_widget_insert_action_group(GTK_WIDGET(self->menu), SNI_GACTION_PREFIX,
                                    self->sni->action_group);
 
-    g_signal_connect(self->button, "clicked",
-                     G_CALLBACK(on_button_clicked_with_menu), self);
+    StatusNotifierService *s = status_notifier_service_get_global();
+    g_signal_connect(s, "status-notifier-item-menu-updated",
+                     G_CALLBACK(on_sni_menu_updated), self);
 }
 
 StatusNotifierItem *indicator_widget_get_sni(IndicatorWidget *self) {
